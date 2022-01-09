268dc9b6f46cf7ff9c028cfc04334664e475f552
jack <jackzhoumine@gmail.com>
2022-01-09 21:15:37 +0800

    添加测试

---
+++ src/tests/index.test.js
@@ -0,0 +1,14 @@
+/*
+ * @Description :
+ * @Date        : 2022-01-09 21:10:50 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2022-01-09 21:14:53 +0800
+ * @LastEditors : JackChou
+ */
+const expect = require('chai').expect
+
+describe('测试index.js', () => {
+  it('should work', () => {
+    expect(1).to.equal(1)
+  })
+})
